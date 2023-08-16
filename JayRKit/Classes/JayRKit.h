e6507736cd8659880f2abbd215dd86be39b1153c
jaySun-ch <szhixiong2022@163.com>
2023-08-16 16:32:33 +0800

    0.1.3 更新pod

---
+++ JayRKit/Classes/JayRKit.h
@@ -0,0 +1,34 @@
+//
+//  JayRKit.h
+//  JayRKit_Example
+//
+//  Created by sunzhixiong on 2023/7/27.
+//  Copyright © 2023 jaySun-ch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HWPanModal.
+FOUNDATION_EXPORT double JayRKitVersionNumber;
+
+//! Project version string for JYHitchModule.
+FOUNDATION_EXPORT const unsigned char JayRKitVersionString[];
+
+#import "UITapGestureRecognizer+JRKit.h"
+#import "UIButton+JRKit.h"
+#import "UIControl+JRKit.h"
+#import "UIView+JRKit.h"
+#import "UILabel+JRKit.h"
+#import "UIViewController+JRKit.h"
+
+#import "NSString+JRKit.h"
+#import "NSObject+JRKit.h"
+#import "NSAttributedString+JRKit.h"
+
+#import "JRMacro.h"
+
+#import "JRPopViewController.h"
+
+#import "JRFillVerifyCodeView.h"
+#import "JRDropTextField.h"
+#import "JRTextField.h"
